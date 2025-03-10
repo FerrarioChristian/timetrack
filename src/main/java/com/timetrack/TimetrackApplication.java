ebaca724ccb4cb47f3314e386fe73beb48ecf185
FerrarioChristian <chri.fer.emi@gmail.com>
2025-03-10 11:27:07 +0100

    Implemented .env for db user and password

--- src/main/java/com/timetrack/TimetrackApplication.java
+++ src/main/java/com/timetrack/TimetrackApplication.java
@@ -1,5 +1,6 @@
 package com.timetrack;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TimetrackApplication {
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(TimetrackApplication.class, args);
 	}
 }
